2cee840d70b0a6be8841f910ac181b53f72a3e52
gigenaMatias <matiasgigena12345@outlook.com>
2024-01-12 17:27:53 -0300

    comienzo de index

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion Ferreteria Rozzo</title>
+    <link rel="icon" type="image/x-icon" href="imagenes/iconos/ferreteria.ico">
+</head>
+<body>
+    Hola mundo
+</body>
+</html>
