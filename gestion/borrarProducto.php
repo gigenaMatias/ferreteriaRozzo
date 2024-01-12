702ec6e0947c7de82c53ece3dba96624196ab461
gigenaMatias <matiasgigena12345@outlook.com>
2024-01-12 18:27:13 -0300

    redireccion borrar producto

--- gestion/borrarProducto.php
+++ gestion/borrarProducto.php
@@ -3,12 +3,9 @@ include('../conexion.php');
 
 $id = $_POST['idProducto'];
 
-print_r($id); //debug
-
-$sql = "DELETE * FROM productos WHERE id = $id";
+$sql = "DELETE FROM productos WHERE id = '".$id."'";
 $resultado = mysqli_query($conexion, $sql);
 
-print_r($resultado);
-
+header("location: productos.php"); //redireccion a productos
 
 ?>
