2485960afd03159abf37ef417b574dc97bf057b0
gigenaMatias <matiasgigena12345@outlook.com>
2024-01-15 20:19:32 -0300

    agregada funcion livesearch, falta entablar

--- gestion/liveSearchProductos.php
+++ gestion/liveSearchProductos.php
@@ -2,30 +2,31 @@
 $q = $_GET['q'];
 include('conexion.php');
 
-$sql="SELECT * FROM productos WHERE nombre LIKE '%".$q."%'";
+$sql = "SELECT * FROM productos WHERE nombre LIKE '%".$q."%'";
 $result = mysqli_query($conexion,$sql);
 
-$row = mysqli_fetch_assoc($result);
+$productosLive = mysqli_fetch_assoc($result);
+//print_r($productosLive);
 
-print_r($row["nombre"]);
-
-echo " <table>
-       <tr>
-        <th>Nombre</th>
-        <th>divisible</th>
-        <th>imagen</th>
-        <th>provedor</th>
-       </tr>";
-while($productos = mysqli_fetch_assoc($result)) {
-    print_r($productos);
+echo "<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+<th>Age</th>
+<th>Hometown</th>
+<th>Job</th>
+</tr>";
+while($row = mysqli_fetch_array($result)) {
   echo "<tr>";
-     echo "<td>" . $row["nombre"] . "</td>";
-     echo "<td>" . $row["divisible"] . "</td>";
-     echo "<td>" . $row["imagen"] . "</td>";
-     echo "<td>" . $row["provedor"] . "</td>";
+  echo "<td>" . $row['nombre'] . "</td>";
+  echo "<td>" . $row['cantidad'] . "</td>";
+  echo "<td>" . $row['divisible'] . "</td>";
+  echo "<td>" . $row['imagen'] . "</td>";
+  echo "<td>" . $row['provedor'] . "</td>";
   echo "</tr>";
 }
 echo "</table>";
+
 mysqli_close($conexion);
 ?>
 </body>
--- gestion/productos.php
+++ gestion/productos.php
@@ -61,7 +61,7 @@ if (mysqli_num_rows($resultado) > 0) {
 </script>
 
   <form>
-  <input type='text' size='30' onkeyup='showResult(this.value)'>
+  <input id='inputBusqueda' type='text' size='30' onkeyup='showResult(this.value)'>
   <div id='livesearch'></div>
   </form>
   <br>
