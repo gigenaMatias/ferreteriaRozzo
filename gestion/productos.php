475fe13855ca74e43de46f9944efc8862fcca8d9
gigenaMatias <matiasgigena12345@outlook.com>
2024-01-17 17:58:16 -0300

    posicionamiento tabla busqueda

--- gestion/productos.php
+++ gestion/productos.php
@@ -12,35 +12,10 @@
   //consulta de productos
   $sql = "SELECT * FROM productos";
   $resultado = mysqli_query($conexion, $sql);
-
-//impresion de productos
-if (mysqli_num_rows($resultado) > 0) { 
-    // output de datos
-    while($fila = mysqli_fetch_assoc($resultado)) {
-      echo 
-       "id: " . $fila["id"].
-       " - nombre: " . $fila["nombre"]. 
-       " - cantidad: " . $fila["cantidad"]. 
-       " - divisible: " . $fila["divisible"].
-       " - imagen: " . $fila["imagen"].
-     //  "<img src='".$fila["imagen"]."'><br>".
-       " - provedor: " . $fila["provedor"]. //tomar nombre provedor de acuerdo a su id
-       "<form action='modificarProducto.php' method='POST'>
-            <input hidden type='number' name='idP' value='".$fila["id"]."'>
-            <button type='submit'>Modificar producto</button>
-        </form>
-        <form action='borrarProducto.php' method='POST'>
-            <input hidden type='number' name='idProducto' value='".$fila["id"]."'>
-            <button type='submit'>Borrar Producto</button>
-        </form><br>";
-    }
-  } else {
-    echo "Sin resultados";
-  }
   
   //busqueda en vivo de productos
 
-  echo"
+ echo"
   
   <script>
     function showResult(str) {
@@ -61,13 +36,11 @@ if (mysqli_num_rows($resultado) > 0) {
 </script>
 
   <form>
-  <input id='inputBusqueda' type='text' size='30' onkeyup='showResult(this.value)'>
+  <input id='inputBusqueda' placeholder='Escriba el nombre del producto a buscar' type='text' size='30' onkeyup='showResult(this.value)'>
   <div id='livesearch'></div>
   </form>
   <br>
   <div id='txtHint'><b>Datos del producto aqui...</b></div>";
-
-
 
   //impresion de productos
   if (mysqli_num_rows($resultado) > 0) { 
