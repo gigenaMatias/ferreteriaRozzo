464d85710e065d1c7960e8c3e960de47bfbb56f8
an47su <sporch44@gmail.com>
2024-01-17 19:32:37 -0300

    archivos

--- gestion/guardarCambiosProducto.php
+++ gestion/guardarCambiosProducto.php
@@ -7,12 +7,24 @@ $cantidad = $_POST['cantidad'];
 $divisible = $_POST['divisible'];
 $imagen = $_POST['imagen'];
 $provedor = $_POST['provedor'];
-$tipoDeImagen = strtolower(pathinfo($imagen,PATHINFO_EXTENSION));
-rename($imagen,'imgSubida/'.$nombre.'.'.$tipoDeImagen);
-//consulta para guardar cambios
-$consulta = "UPDATE productos SET nombre= '".$nombre."', cantidad= '".$cantidad."', divisible= '".$divisible."' ,
-imagen= '".$nombre.'.'.$tipoDeImagen."', provedor= '".$provedor."' WHERE id= ".$id."";
-$resultado = mysqli_query($conexion,$consulta);
+$sql_verify = "SELECT nombre FROM productos WHERE nombre = '$nombre'";
+$querys = mysqli_query($conexion,$sql_verify);
+if (mysqli_num_rows($querys) == 0){
+    $tipoDeImagen = strtolower(pathinfo($imagen,PATHINFO_EXTENSION));
+    rename($imagen,'imgSubida/'.$nombre.'.'.$tipoDeImagen);
+    $consulta = "UPDATE productos SET nombre= '".$nombre."', cantidad= '".$cantidad."', divisible= '".$divisible."' ,
+    imagen= '".'imgSubida/'.$nombre.'.'.$tipoDeImagen."', provedor= '".$provedor."' WHERE id= ".$id."";
+    $resultado = mysqli_query($conexion,$consulta);
+    echo'<script type="text/javascript">
+    alert("MODIFICADO CORRECTAMENTE");
+    window.location.href="productos.php";
+    </script>';
+}
+else{
+    echo'<script type="text/javascript">
+    alert("NOMBRE REGISTRADO, INGRESE OTRO");
+    window.location.href="productos.php";
+    </script>';
+}
 
-header("location: productos.php");
 ?>
