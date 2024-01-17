4cac98997c0fd7ae3b406466dafe49dc5c3e3d9a
gigenaMatias <matiasgigena12345@outlook.com>
2024-01-17 17:53:30 -0300

    livesearch en productos

--- gestion/liveSearchProductos.php
+++ gestion/liveSearchProductos.php
@@ -6,7 +6,6 @@ $sql = "SELECT * FROM productos WHERE nombre LIKE '%".$q."%'";
 $result = mysqli_query($conexion,$sql);
 
 $productosLive = mysqli_fetch_assoc($result);
-//print_r($productosLive);
 
 echo "<table>
 <tr>
@@ -16,13 +15,20 @@ echo "<table>
 <th>Hometown</th>
 <th>Job</th>
 </tr>";
-while($row = mysqli_fetch_array($result)) {
+echo "<tr>";
+  echo "<td>".$productosLive['nombre']."</td>"; //mostrar 1r elemento
+  echo "<td>".$productosLive['cantidad']."</td>";
+  echo "<td>".$productosLive['divisible']."</td>";
+  echo "<td>".$productosLive['imagen']."</td>";
+  echo "<td>".$productosLive['provedor']."</td>";
+  echo "</tr>";
+while($row = mysqli_fetch_array($result)) { //mostrar despues del 1er elemento
   echo "<tr>";
-  echo "<td>" . $row['nombre'] . "</td>";
-  echo "<td>" . $row['cantidad'] . "</td>";
-  echo "<td>" . $row['divisible'] . "</td>";
-  echo "<td>" . $row['imagen'] . "</td>";
-  echo "<td>" . $row['provedor'] . "</td>";
+  echo "<td>".$row['nombre']."</td>";
+  echo "<td>".$row['cantidad']."</td>";
+  echo "<td>".$row['divisible']."</td>";
+  echo "<td>".$row['imagen']."</td>";
+  echo "<td>".$row['provedor']."</td>";
   echo "</tr>";
 }
 echo "</table>";
